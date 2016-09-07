63a719b8a596df522ef606efe3e99002c3d15688
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-09-07 19:50:49 +0530

    QLearning no deadline

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -15,6 +15,15 @@ class LearningAgent(Agent):
         self.red_light_violations = []
         self.planner_noncompliance = []
         
+        self.qLearnTable = {}
+        self.prevActionIndex = None
+        self.prevReward = 0
+        self.prevState = None
+        
+        self.alpha = 0.5
+        self.gamma = 0.1
+        self.epsilon = 1
+        
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -22,6 +31,10 @@ class LearningAgent(Agent):
        
         self.red_light_violations.append(0)
         self.planner_noncompliance.append(0)
+        
+        self.prevActionIndex = None
+        self.prevReward = 0
+        self.prevState = None
 
 
     def update(self, t):
@@ -31,24 +44,56 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        self.state = (inputs['light'], inputs['oncoming'], inputs['left'], self.next_waypoint)
+                    
         # TODO: Select action according to your policy
-        action = random.choice(self.env.valid_actions)
 
+        action = self.selectBestAction()
+        
         # Execute action and get reward
         reward = self.env.act(self, action)
         if reward == -1.0:
             self.red_light_violations[len(self.red_light_violations) - 1] += 1
         elif reward == -0.5:
             self.planner_noncompliance[len(self.planner_noncompliance) - 1] += 1
+        
 
         # TODO: Learn policy based on state, action, reward
+       
+        if self.prevState != None:
+            qMax = max(self.qLearnTable[self.state])
+            prevQ = self.qLearnTable[self.prevState][self.prevActionIndex] 
+            newQ = prevQ + self.alpha *(self.prevReward + self.gamma * (qMax - prevQ))
+            self.qLearnTable[self.prevState][self.prevActionIndex] = newQ
+            #print 'prevQ:', prevQ, 'newQ: ', newQ
+            
+        self.prevState = self.state
+        self.prevActionIndex = self.env.valid_actions.index(action)
+        self.prevReward = reward
+        
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
  
         if self.planner.next_waypoint() == None :
              self.success += 1
 
+    def selectBestAction(self):
+        self.epsilon = self.epsilon * 0.999
+        #print self.epsilon
+        if self.state not in self.qLearnTable.keys():
+            # new state, initialize to zero
+            self.qLearnTable[self.state] = [0,0,0,0]
+            #choose a random action
+            return random.choice(self.env.valid_actions)
+            
+        if random.random() < self.epsilon:
+            # explore by choosing a random action
+            return random.choice(self.env.valid_actions)
+        
+        # find the best action for current state
+        maxQ = max(self.qLearnTable[self.state])
+        index = self.qLearnTable[self.state].index(maxQ)
+        return self.env.valid_actions[index]
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -70,12 +115,14 @@ def run():
     import matplotlib.pyplot as plt
     plt.plot(a.red_light_violations, label='red light violations', color='r')
     plt.plot(a.planner_noncompliance, label = 'planner noncompliance', color='b')
+    
     plt.legend()
     plt.xlabel('Trials')
     
-    plt.title('Random actions, no deadline. Success rate %d%%' % a.success)
+    plt.title('Q-Learning, no deadline. Success rate %d%%' % a.success)
     #plt.show()
-    plt.savefig('radomAction.png')
+    plt.savefig('QLearning.png')
+
 
 
 if __name__ == '__main__':
